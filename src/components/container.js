c197434956ee3818f7c62bde7479ca9e28129c36
Jeroen Slor <jeroen@jeroenslor.com>
2018-01-12 15:54:09 +0100

    Moved to build output

--- src/components/container.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default ({ children, style, className }) => (
-    <div className={className} style={{ margin: '0 auto', padding: '0 20px', maxWidth: 1360, ...style }}>
-        { children }
-    </div>
-)
